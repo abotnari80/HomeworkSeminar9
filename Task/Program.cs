2ec0b55ce73e2ec45fbc01e0c248b3e9c52bc47f
abotnari80 <abotnari80@gmail.com>
2022-04-14 00:20:56 +0300

    Задача

---
+++ Task/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите количество строк в треугильнике");
+int num = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 0; i < num; i++)
+{
+    for (int j = num; j > i; j--)
+    {
+        Console.Write(" ");
+    }
+    int value = 1;
+    for (int j = 0; j <= i; j++)
+    {
+        Console.Write(value + " ");
+        value = value * (i - j) / (j + 1);
+    }
+    Console.WriteLine();
+}
